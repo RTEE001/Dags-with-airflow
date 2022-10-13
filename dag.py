7ba84320d508d23d0b3cc1eaa1c0f899211caefc
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-13 23:53:47 +0200

    initialized dag

--- dag.py
+++ dag.py
@@ -2,3 +2,9 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+
+with DAG("my_dag", # Dag id
+start_date=datetime(2020, 1 ,1), # start date, the 1st of January 2021 
+schedule_interval='@daily',  # Cron expression, here it is a preset of Airflow, @daily means once every day.
+catchup=False  # Catchup 
+) as dag:
