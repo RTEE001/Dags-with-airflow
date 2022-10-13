fe86a20b6a2147d9a7d852ab447e49d4f7fcc025
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-13 23:53:26 +0200

    initialized dag

--- dag.py
+++ dag.py
@@ -1,6 +1,4 @@
-# We'll start by importing the DAG object
 from airflow import DAG
-# We need to import the operators used in our tasks
-from airflow.operators.bash_operator import BashOperator
-# We then import the days_ago function
-from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
