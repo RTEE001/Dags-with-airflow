3ec45dbcc6cff0c66c1ef0bbe5c22d8ac21fc5ee
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-16 11:55:53 +0200

    added fucntion to retrieve all open prs

--- dag.py
+++ dag.py
@@ -2,9 +2,30 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+import requests
 
-with DAG("my_dag", # Dag id
-start_date=datetime(2020, 1 ,1), # start date, the 1st of January 2021 
-schedule_interval='@daily',  # Cron expression, here it is a preset of Airflow, @daily means once every day.
-catchup=False  # Catchup 
-) as dag:
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+OWNER = os.getenv("OWNER")
+
+def get_all_repos():
+    accessible_repos_url = f" https://api.github.com/user/repos?per_page=100"
+    accessible_repos_url_response = requests.get(accessible_repos_url, auth=(OWNER, TOKEN))
+    accessible_repos = accessible_repos_url_response.json()
+    repos_with_open_prs_details = []
+    for each_object in accessible_repos:
+        url = f"{each_object['url']}/pulls"
+        url_response = requests.get(url, auth=(OWNER, TOKEN))
+        repos_with_open_prs = url_response.json()
+        
+        for each_object in repos_with_open_prs:
+            if len(each_object) !=0:
+                repos_with_open_prs_details.append(
+                    each_object
+                )
+
+    return repos_with_open_prs_details
+
+print(get_all_repos())
