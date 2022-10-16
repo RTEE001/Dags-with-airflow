8c05409f07d9c5d693afa8565213033c8550a687
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-16 17:46:19 +0200

    working on timestamps

--- dag.py
+++ dag.py
@@ -10,22 +10,44 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 OWNER = os.getenv("OWNER")
 
+
 def get_all_repos():
-    accessible_repos_url = f" https://api.github.com/user/repos?per_page=100"
-    accessible_repos_url_response = requests.get(accessible_repos_url, auth=(OWNER, TOKEN))
+    accessible_repos_url = f"https://api.github.com/user/repos?per_page=100"
+    accessible_repos_url_response = requests.get(
+        accessible_repos_url, auth=(OWNER, TOKEN)
+    )
     accessible_repos = accessible_repos_url_response.json()
     repos_with_open_prs_details = []
     for each_object in accessible_repos:
         url = f"{each_object['url']}/pulls"
         url_response = requests.get(url, auth=(OWNER, TOKEN))
         repos_with_open_prs = url_response.json()
-        
-        for each_object in repos_with_open_prs:
-            if len(each_object) !=0:
-                repos_with_open_prs_details.append(
-                    each_object
-                )
+
+        for each_object_ in repos_with_open_prs:
+            if len(each_object_) != 0:
+                repos_with_open_prs_details.append(each_object_['url'])
 
     return repos_with_open_prs_details
 
-print(get_all_repos())
+def get_timestamps():
+    open_with_no_reviews =[]
+    open_prs_with_reviews = []
+    for each_pr in get_all_repos():
+        comments_url = f"{each_pr}/reviews"
+        url_response = requests.get(comments_url, auth=(OWNER, TOKEN))
+        comments = url_response.json()
+        if len(comments)==0:
+            url = each_pr
+            url_response = requests.get(comments_url, auth=(OWNER, TOKEN))
+            comments = url_response.json()
+            open_with_no_reviews.append(
+                comments
+            )
+            continue
+        else:
+            open_prs_with_reviews.append({
+                'submitted_at': comments["submitted_at"]
+            })
+    return open_with_no_reviews
+
+print(get_timestamps())
