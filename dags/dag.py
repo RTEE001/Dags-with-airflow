226d342b8dd0976a2ce90a80d9762779e4d8e700
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-10-20 18:29:31 +0200

    added task 1

--- dags/dag.py
+++ dags/dag.py
@@ -1,13 +1,34 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates import days_ago 
+from airflow.utils.dates import days_ago , timedelta
 import os
-from dotenv import load_dotenv, find_dotenv
+# from dotenv import load_dotenv
 import requests
 
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
-OWNER = os.getenv("OWNER")
+# load_dotenv()
+# TOKEN = os.getenv("TOKEN")
+# OWNER = os.getenv("OWNER")
+
+
+def  get_all_open_prs():
+
+    accessible_repos_url = f"https://api.github.com/user/repos?per_page=100"
+    accessible_repos_url_response = requests.get(
+        accessible_repos_url, auth=(OWNER, TOKEN)
+    )
+    accessible_repos = accessible_repos_url_response.json()
+    repos_with_open_prs_details = []
+    for each_object in accessible_repos:
+        url = f"{each_object['url']}/pulls"
+        url_response = requests.get(url, auth=(OWNER, TOKEN))
+        repos_with_open_prs = url_response.json()
+
+        for each_object_ in repos_with_open_prs:
+            if len(each_object_) != 0:
+                repos_with_open_prs_details.append(each_object_['url'])
+
+    return repos_with_open_prs_details
+
 
 default_args = {
     'owner' :'airflow',
@@ -18,7 +39,7 @@ pr_dag = DAG(
     'pr_filter',
     default_args = default_args, 
     description =  'notifies the user which pull requests need attention',
-    schedule_interval = timedelta(days =1), 
+    schedule = timedelta(days =1), 
     catchup = False
 )
 
@@ -29,38 +50,22 @@ task1 = PythonOperator(
 )
 
 
-task2 = PythonOperator(
-    task_id = 'get_timestamps',
-    python_callable = get_timestamps,
-    dag = pr_dag, 
-)
+# task2 = PythonOperator(
+#     task_id = 'get_timestamps',
+#     python_callable = get_timestamps,
+#     dag = pr_dag, 
+# )
 
-task3 = PythonOperator(
-    task_id = 'send_email',
-    python_callable = send_email,
-    dag = pr_dag, 
-)
+# task3 = PythonOperator(
+#     task_id = 'send_email',
+#     python_callable = send_email,
+#     dag = pr_dag, 
+# )
 
-task1 >> task2 >> task3
+# task1 >> task2 >> task3
 
 
-# def get_all_repos():
-#     accessible_repos_url = f"https://api.github.com/user/repos?per_page=100"
-#     accessible_repos_url_response = requests.get(
-#         accessible_repos_url, auth=(OWNER, TOKEN)
-#     )
-#     accessible_repos = accessible_repos_url_response.json()
-#     repos_with_open_prs_details = []
-#     for each_object in accessible_repos:
-#         url = f"{each_object['url']}/pulls"
-#         url_response = requests.get(url, auth=(OWNER, TOKEN))
-#         repos_with_open_prs = url_response.json()
 
-#         for each_object_ in repos_with_open_prs:
-#             if len(each_object_) != 0:
-#                 repos_with_open_prs_details.append(each_object_['url'])
-
-#     return repos_with_open_prs_details
 
 # def get_timestamps():
 #     open_with_no_reviews =[]
