1b7801b214487206f21bdfb8aa093db6fe5612fb
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-11-14 10:35:13 +0200

    used black formatter

--- dags/helper_functions.py
+++ dags/helper_functions.py
@@ -1,25 +1,38 @@
 import requests
 from airflow.models import Variable
-import json 
+import json
 from airflow.hooks.postgres_hook import PostgresHook
 
 TOKEN = Variable.get("TOKEN")
 USERNAME = Variable.get("USERNAME")
 GITHUB_API_LINK = "https://api.github.com/user/repos?per_page=100"
 
+
 def read_url(url):
     url_response = requests.get(url, auth=(USERNAME, TOKEN))
     response = url_response.json()
     return response
 
+
 def extract_from_db(table_name):
-        request = f"SELECT * FROM {table_name}"
-        pg_hook = PostgresHook(postgres_conn_id = "postgres_db", schema = "airflow")
-        connection = pg_hook.get_conn()
-        cursor = connection.cursor()
-        cursor.execute(request)
-        result = cursor.fetchall()
-        return result
+    request = f"SELECT * FROM {table_name}"
+    pg_hook = PostgresHook(postgres_conn_id="postgres_db", schema="airflow")
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+    cursor.execute(request)
+    result = cursor.fetchall()
+    return result
+
+
+def create_and_populate_table(table_name, value):
+    return f"""
+            CREATE TABLE IF NOT EXISTS {table_name}(
+                name VARCHAR(10000)
+            );
+            INSERT INTO {table_name}(name)
+            VALUES ('{value}')
+        """
+
 
 def get_timestamps(pr_list):
     timestamps = []
@@ -39,17 +52,19 @@ def get_timestamps(pr_list):
                     timestamps.append({key: k["submitted_at"]})
     return timestamps
 
+
 def filter_timestamps_by_latest_time(pr_list):
-        result_dict = {}
-        for item in pr_list:
-            key = list(item.keys())[0]
-            if key in result_dict:
-                if item[key] > result_dict[key][key]:
-                    result_dict.update({key: item})
-            else:
+    result_dict = {}
+    for item in pr_list:
+        key = list(item.keys())[0]
+        if key in result_dict:
+            if item[key] > result_dict[key][key]:
                 result_dict.update({key: item})
-        result_list = [v for k, v in result_dict.items()]
-        return result_list
+        else:
+            result_dict.update({key: item})
+    result_list = [v for k, v in result_dict.items()]
+    return result_list
+
 
 def sort_timestamps(pr_list):
     values = []
@@ -62,15 +77,16 @@ def sort_timestamps(pr_list):
 
     for each_dict in pr_list:
         insert_index = values.index(list(each_dict.values())[0])
-        sorted_timestamps.insert(insert_index, {
-            list(each_dict.keys())[0] : values[insert_index]
-        })
+        sorted_timestamps.insert(
+            insert_index, {list(each_dict.keys())[0]: values[insert_index]}
+        )
     return sorted_timestamps
+
 
 def get_top_five_prs(pr_list):
     urgent_prs = []
-    if len(pr_list) >5:
+    if len(pr_list) > 5:
         for i in range(5):
             urgent_prs.append(pr_list[i])
-        return urgent_prs     
+        return urgent_prs
     return pr_list
--- dags/taskflow_dag.py
+++ dags/taskflow_dag.py
@@ -7,45 +7,49 @@ import json
 GITHUB_API_LINK = "https://api.github.com/user/repos?per_page=100"
 
 default_args = {
-    'owner' :'airflow',
-    'start_date': datetime(2022, 10, 1),
+    "owner": "airflow",
+    "start_date": datetime(2022, 10, 1),
 }
 
-@dag(
-    schedule = timedelta(minutes = 10),
-    default_args = default_args,
-    catchup=False,
-    description =  'notifies the user which pull requests need attention',
-    tags=['pr'],
-)
 
+@dag(
+    schedule=timedelta(minutes=10),
+    default_args=default_args,
+    catchup=False,
+    description="notifies the user which pull requests need attention",
+    tags=["pr"],
+)
 def pr_filter():
     @task
     def get_all_open_prs():
-        from helper_functions import read_url
+        from helper_functions import read_url, create_and_populate_table
+
         all_pr_response = read_url(GITHUB_API_LINK)
         for each_object in all_pr_response:
             repos_with_open_prs = read_url(f"{each_object['url']}/pulls")
             for each_object_ in repos_with_open_prs:
                 if len(each_object_) != 0:
-                    json_str = json.dumps({each_object_['html_url']: each_object_['url']})
+                    json_str_prs = json.dumps(
+                        {each_object_["html_url"]: each_object_["url"]}
+                    )
                     create_table = PostgresOperator(
-                    task_id = "create_table",
-                    postgres_conn_id = "postgres_db",
-                    sql = f"""
-                        CREATE TABLE IF NOT EXISTS pr(
-                            name VARCHAR(10000)
-                        );
-                        INSERT INTO pr (name)
-                        VALUES ('{json_str}')
-                    """,
-                )
+                        task_id="create_table",
+                        postgres_conn_id="postgres_db",
+                        sql=create_and_populate_table("pr", json_str_prs),
+                    )
                     create_table.execute(dict())
-                  
-        
+
     @task
     def consolidate_pull_requests():
-        from helper_functions import get_timestamps, filter_timestamps_by_latest_time, sort_timestamps, get_top_five_prs, extract_from_db
+        from helper_functions import (
+            get_timestamps,
+            filter_timestamps_by_latest_time,
+            sort_timestamps,
+            get_top_five_prs,
+            extract_from_db,
+            create_and_populate_table,
+        )
+
         result = extract_from_db("pr")
         timestamps = get_timestamps(result)
         filtered_timestamps = filter_timestamps_by_latest_time(timestamps)
@@ -58,33 +62,28 @@ def pr_filter():
         links = "\n\n".join(links)
 
         put_data = PostgresOperator(
-        task_id = "put_data",
-        postgres_conn_id = "postgres_db",
-        sql = f"""
-            CREATE TABLE IF NOT EXISTS pr_timestamps(
-                name VARCHAR(10000)
-            );
-            INSERT INTO pr_timestamps(name)
-            VALUES ('{links}')
-        """,
-    )
+            task_id="put_data",
+            postgres_conn_id="postgres_db",
+            sql=create_and_populate_table("pr_timestamps", links),
+        )
         put_data.execute(dict())
- 
+
     @task
     def send_email():
 
         from helper_functions import extract_from_db
         import smtplib
         from email.mime.text import MIMEText
+
         SMTP_SERVER = Variable.get("SMTP_SERVER")
         SMTP_PORT = Variable.get("SMTP_PORT")
         SENDER_EMAIL_ADDRESS = Variable.get("SENDER_EMAIL_ADDRESS")
         RECEIPIENT_EMAIL_ADDRESS = Variable.get("RECEIPIENT_EMAIL_ADDRESS")
         SMTP_PASSWORD = Variable.get("SMTP_PASSWORD")
-        
+
         subject = "Urgent pull requests that need attention"
         message = extract_from_db("pr_timestamps")
-        message = '\n'.join([str(x) for t in message for x in t])
+        message = "\n".join([str(x) for t in message for x in t])
         body = message
 
         msg = MIMEText(body)
@@ -99,6 +98,6 @@ def pr_filter():
         smtp.quit()
 
     get_all_open_prs() >> consolidate_pull_requests() >> send_email()
-    
+
 
 pr_filter()
